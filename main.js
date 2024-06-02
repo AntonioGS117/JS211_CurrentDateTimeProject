c68fea1fdb0759a78d065122d11397748bb3dc52
AntonioGS117 <antonio.gasconsalazar@gmail.com>
2024-06-02 16:16:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15,14 +15,37 @@ const displayDate = () => {
  
 
 // Write a JavaScript program to convert a number to a string.
+let number = 123;
+let str = String(number);
+console.log(str); 
+
 
 
 
 // Write a JavaScript program to convert a string to the number.
-
+let a = "777";
+parseInt(a)+ 223;
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
+  function checkDatatypes(element) {
+    if (element === true || element === false) { console.log("Boolean")
+  }
+if (element === null ) {console.log("Null")}
+if (element === undefined) {console.log("Undefined")}
+
+if (typeof element === 'number') {
+  console.log(element + 'is a number');
+} else if (typeof element === 'string') {
+    console.log(element + 'is a string');
+  } else {
+    console.log(element + ' is neither a number nor a string');
+  }
+}
+if (element === NaN) {console.log("Nan")}
+if (element === string) {console.log("String")}
+}
+  console.log(checkDatatypes(null))
   // * Boolean
   // * Null
   // * Undefined
@@ -34,18 +57,50 @@ const displayDate = () => {
 
   
 // Write a JavaScript program that adds 2 numbers together.
+function addNumbers(num1, num2) {
+  return num1 + num2;}
+
+  let number1 = 25;
+  let number2 = 25;
+  let sum = addNumbers(number1, number2);
+
+console.log("The sum of", number1, "and", number2, "is", sum);
 
 
 
 // Write a JavaScript program that runs only when 2 things are true.
+var x = 26;
+var y = 26;
+var z = 28;
+
+function trueTwo(a, b, c){
+  if(a === b && c > b){
+    return true;}
+  }
+console.log(trueTwo(x, y, z));
 
 
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
 
+function runWhenTrue(condition1, condition2) {
+  if (condition1 || condition2) {
+      console.log("One of the conditions is true. Running the program.");
+  } else {
+      console.log("Neither condition is true. The program will not run.");
+  }
+}
+let condition1 = true;
+let condition2 = false;
 
 
-// Write a JavaScript program that runs when both things are not true.  
+// Write a JavaScript program that runs when both things are not true. 
+
+
+
+
+
+
 
 // ***************************
 //         PART TWO
